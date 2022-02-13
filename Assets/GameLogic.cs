86c502c094380a8721ed71b3169649d2f55cb534
Cevdet Talha Çevik <48892944+cevdettalhacevik@users.noreply.github.com>
2022-02-14 00:26:36 +0300

    kda5 pc tanıtma
    bana verilen senaryoda gerçekleştirilmesi istenen tüm durumları gerçekleştirdim, ilk oyun deneyimimdi yaparken eğlendim

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    GameObject gameobjesi;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 click2D = new Vector2(clickpos.x,clickpos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(click2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                gameobjesi = hit.collider.gameObject;
+                gameobjesi.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+  
+}
---
+++ Assets/GameLogic2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic2 : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource [] audisource;
+    string adı;
+    private AudioSource[] audiobulucu;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audiobulucu = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 click2D = new Vector2(clickpos.x, clickpos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(click2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                adı = hit.collider.gameObject.name;
+                foreach (AudioSource bulunan in audiobulucu)
+                {
+                    bulunan.Stop();
+                }
+                switch (adı)
+                {
+                    case "Monitor":
+                        audisource[0].Play();
+                        break;
+                    case "Kasa":
+                        audisource[1].Play();
+                        break;
+                    case "Mouse":
+                        audisource[2].Play();
+                        
+                        break;
+                    case "Klavye":
+                        audisource[3].Play();
+                        break;
+                    
+                }
+            }
+        }
+    }
+}
